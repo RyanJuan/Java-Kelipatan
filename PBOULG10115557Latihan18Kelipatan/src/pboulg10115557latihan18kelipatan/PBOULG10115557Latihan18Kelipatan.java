c39cc432221ed07b5b39863b2c019afa2d997cd4
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-10-10 21:25:16 +0700

    Add files via upload

---
+++ PBOULG10115557Latihan18Kelipatan/src/pboulg10115557latihan18kelipatan/PBOULG10115557Latihan18Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg10115557latihan18kelipatan;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 2 / latihan 18
+ */
+public class PBOULG10115557Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          for (double bil= 3.5; bil<=35; bil+=3.5){
+                System.out.println(bil);
+            }
+        }   
+    }
+
